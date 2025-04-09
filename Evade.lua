22ad08f65271c86d6a545d3bd0505669559edded
zxcFedka <barsuk20367@gmail.com>
2025-04-09 20:31:57 +0400

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -2,8 +2,6 @@ if not game.PlaceId == 9872472334 then return end
 
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua'))()
 
-local player = game.Players.LocalPlayer
-
 local Window = Rayfield:CreateWindow({
     Name = "Evade",
     Icon = 0,
@@ -34,6 +32,26 @@ local Window = Rayfield:CreateWindow({
     }
 })
 
+local player = game.Players.LocalPlayer
+local PlayerGui = player.PlayerGui
+
 local MainTab = Window:CreateTab("Home", nil)
 local MineSection = MainTab:CreateSection("Main")
 MineSection:Set("Main")
+
+local Divider = MainTab:CreateDivider()
+
+local function Gui(enable)
+    for i,gui in PlayerGui:GetChildren() do
+        gui.Enabled = not fromSave
+    end
+end
+
+local Toggle = MainTab:CreateToggle({
+    Name = "Gui",
+    CurrentValue = false,
+    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        Gui(Value)
+    end,
+ })
